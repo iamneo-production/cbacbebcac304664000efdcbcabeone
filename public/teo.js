13872556cac08153c8493097327c66c9f060622e
Iamneo <techadmin@iamneo.ai>
2023-09-10 05:38:05 +0000

    Backup Commit

---
+++ public/teo.js
@@ -0,0 +1,13 @@
+function myfunc(){
+    var b1,b2,b3,b4,b5,b6,b7,b8,b9;
+    b1=document.getElementById("b1").ariaValueMax;
+    b1=document.getElementById("b2").ariaValueMax;
+    b1=document.getElementById("b3").ariaValueMax;
+    b1=document.getElementById("b4").ariaValueMax;
+    b1=document.getElementById("b5").ariaValueMax;
+    b1=document.getElementById("b6").ariaValueMax;
+    b1=document.getElementById("b7").ariaValueMax;
+    b1=document.getElementById("b8").ariaValueMax;
+    b1=document.getElementById("b9").ariaValueMax;
+    var b1btn, b2btn, b3btn, b4btn, b5btn, b6btn, b7btn, 
+}  
