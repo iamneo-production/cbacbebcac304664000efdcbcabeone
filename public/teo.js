fda79df3aa221885e8d6ec7f20b37aeb1020d57f
Iamneo <techadmin@iamneo.ai>
2023-09-10 05:53:05 +0000

    Backup Commit

--- public/teo.js
+++ public/teo.js
@@ -35,3 +35,30 @@ function myfunc(){
         b2btn.style.color ="red";
         b3btn.style.color ="red";
 }  
+else if ((b1=="x"|| b1="X")&&(b4== "x" ||b4=="x")&&(b7=="x||b7=="X"))
+        document.getElementById("print")
+             .innerHTML ="Player X won";
+        b2btn.disabled = true;
+        b3btn.disabled = true;
+        b5btn.disabled = true;
+        b6btn.disabled = true;
+        b8btn.disabled = true;
+        b9btn.disabled = true;
+
+        b1btn.style.color ="red";
+        b4btn.style.color ="red";
+        b7btn.style.color ="red";
+}
+esle if ((b7=="x"|| b1="X")&&(b2== "x" ||b2=="x")&&(b3=="x||b3=="X"))
+        document.getElementById("print")
+             .innerHTML ="Player X won";
+        b4btn.disabled = true;
+        b5btn.disabled = true;
+        b6btn.disabled = true;
+        b7btn.disabled = true;
+        b8btn.disabled = true;
+        b9btn.disabled = true;
+
+b1btn.style.color ="red";
+b2btn.style.color ="red";
+b3btn.style.color ="red";
